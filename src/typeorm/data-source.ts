c8d3ce0f83eb06c772c473cf4ff223b764c41bcf
Thais Kotovicz <thais_kotovicz@hotmail.com>
2024-03-18 20:07:39 -0300

    build: DB and TypeORM config

---
+++ src/typeorm/data-source.ts
@@ -0,0 +1,21 @@
+import * as dotenv from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenv.config({
+  path: process.env.ENV === 'test' ? '.env.test' : '.env',
+});
+
+const options: DataSourceOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: true,
+  migrationsRun: true,
+  migrations: [`${__dirname}/migrations/**/*.ts`],
+};
+
+export const dataSource = new DataSource(options);
