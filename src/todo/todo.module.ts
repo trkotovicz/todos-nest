45b0ad638535d714e9935b108d651a16eff38b8a
Thais Kotovicz <thais_kotovicz@hotmail.com>
2024-03-20 15:16:55 -0300

    fix: importts at user and todo modules

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,13 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoEntity } from './entity/todo.entity';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
+import { AuthModule } from '../auth/auth.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TodoEntity])],
+  imports: [
+    TypeOrmModule.forFeature([TodoEntity]),
+    forwardRef(() => AuthModule),
+    forwardRef(() => UserModule),
+  ],
   controllers: [TodoController],
   providers: [TodoService],
-  exports: [],
+  exports: [TodoService],
 })
 export class TodoModule {}
